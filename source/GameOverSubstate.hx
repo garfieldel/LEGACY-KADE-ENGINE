fb869707bfa27881bbcb4ed77dcfbfd53c447441
garfieldel <closetlionel@gmail.com>
2026-05-01 22:39:01 -0500

    buttons

--- source/GameOverSubstate.hx
+++ source/GameOverSubstate.hx
@@ -66,18 +66,22 @@ class GameOverSubstate extends MusicBeatSubstate
 		FlxG.camera.target = null;
 
 		bf.playAnim('firstDeath');
+
+		#if mobile
+		addVirtualPad(NONE, A_B);
+		#end
 	}
 
 	override function update(elapsed:Float)
 	{
 		super.update(elapsed);
 
-		if (controls.ACCEPT)
+		if (controls.ACCEPT #if mobile || virtualPad.buttonA.justPressed #end)
 		{
 			endBullshit();
 		}
 
-		if (controls.BACK)
+		if (controls.BACK #if mobile || virtualPad.buttonB.justPressed #end)
 		{
 			FlxG.sound.music.stop();
 
